f76d92f32ebac3893da7deccba4389b14760c2be
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2024-07-27 18:31:14 -0600

    -- NOVEDADES --  - NAVBAR MEJORADA  - ICONOS EN TABLAS

--- src/components/atoms/TableActions.jsx
+++ src/components/atoms/TableActions.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 
 const TableActions = ({ onEdit, onDelete }) => {
   return (
-    <td className="px-4 py-2 border-gray-100 text-center border-b-2">
-      <button className="px-2 py-1 bg-blue-500 text-white rounded" onClick={onEdit}>
-        Editar
+    <td className="px-4 py-2 border-gray-200 text-center border-b-2">
+      <button className="px-2   text-white" onClick={onEdit}>
+        <FaEdit className="mr-1 fill-black  " />
+        
       </button>
-      <button className="px-2 py-1 bg-red-500 text-white rounded ml-2" onClick={onDelete}>
-        Borrar
+      <button className="px-2 text-white" onClick={onDelete}>
+        <FaTrashAlt className="fill-black"/>
+        
       </button>
     </td>
   );
